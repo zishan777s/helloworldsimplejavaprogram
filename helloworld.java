843c71189ff864b416434db21ea12eafa4f4458c
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2021-11-08 09:01:17 +0530

    UPDATE THE NAMES

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch21 team");
-      System.out.println("Hello Batch batch16 and batch17 1 people with suneel last ci for today and its easy ");
+      System.out.println("Hello Batch batch21 WITH BALA LOKESH PREETHAN KEVIN STUDENTS RUNNING ci JOB ");
     }  
 } 
