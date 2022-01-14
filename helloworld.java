d877b3e1645734cd33290ea734c2032f6762247e
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2022-01-14 09:12:38 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch22 team");
-      System.out.println("Hello Batch batch22 WITH FOR STUDENTS RUNNING ci JOB ");
+      System.out.println("Hello Batch batch22 WITH FOR STUDENTS RUNNING in front of vinesh and prasad and maniB ");
     }  
 } 
