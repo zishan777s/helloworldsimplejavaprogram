6a2790f5ed849441a11413d1cb3ffaf5d6c4d09c
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2022-03-04 19:43:23 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch24 team");
-      System.out.println("Hello Batch batch24 WITH FOR STUDENTS RUNNING in front of Rayees and Prashant and and Priya is tying the msg  doining the CI and 1 changeconfiguration ");
+      System.out.println("Hello Batch batch24 WITH FOR STUDENTS RUNNING in front of Rayees and Prashant and and last ci test with harshita silent girl ");
     }  
 } 
