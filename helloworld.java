825887b5f9691eacfa3c3d6f468b92b890d091bd
zishan777s <68671471+zishan777s@users.noreply.github.com>
2022-05-05 14:27:25 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch26 team");
-      System.out.println("Hello Batch batch26 WITH FOR STUDENTS RUNNING in front of kalyani shilpi abhishek and added the CI configuration in front of supriya and ritu as soon as code comits");
+      System.out.println("Hello Batch batch26 WITH FOR STUDENTS RUNNING in front of zishan and added the CI configuration in front of supriya and ritu as soon as code comits");
     }  
 } 
